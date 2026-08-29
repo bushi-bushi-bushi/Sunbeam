c4559deb18bafeccb11c9724da4da2ad17153917
bushi-bushi-bushi <hibakhan1408@gmail.com>
2026-08-29 01:27:45 -0700

    Add files via upload

---
+++ Day_Perfecter.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#title for webpage
+st.title('Plan Your Perfect Day!🎉')
+
+#creating sidebar for multipage website
+st.sidebar.success("Only a perfect plan leads to a perfect day!")
+
+st.markdown(
+    """
+    Perfect Your Day🎉 is my very first project made to solve a problem I'm not sure many people have. 
+    ### What does it do?
+    - Allows users to upload their clunky and unreadable .csv day planners. 
+    - Displays the needed rows in a clear manner
+    - Allows users to check if multiple things are scheduled at the same time.
+    - Ta Da!🎊 your day is perfected with a perfect day planner.
+    
+    **👈 head over to the sidebar to see it in action** 
+        
+        
+"""
+)
+
+
+
+
+
+
